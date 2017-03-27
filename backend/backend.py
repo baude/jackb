8b8f16697e3cdeeb0c48c81bed1509f64e35f443
Brent Baude <bbaude@redhat.com>
2017-03-27 18:09:07 -0500

    Added in GPIO methods for on, off, status, and blink.

--- backend/backend.py
+++ backend/backend.py
@@ -1,13 +1,46 @@
 from flask import Flask, render_template
+from gpiozero import LED
+
 app = Flask(__name__)
+
+LIGHT = LED(17)
 
 @app.route('/hello')
 def hello_flask():
-   return 'Hello from flask\n'
+    return 'Hello from flask\n'
 
 @app.route('/hello/<user>')
 def hello_name(user):
-   return render_template('hello.html', name = user)
+    return render_template('hello.html', name = user)
+
+
+@app.route('/light/on')
+def light_on():
+    if not LIGHT.is_active:
+        LIGHT.on()
+        return "turned light on"
+    return "light is already on"
+
+
+@app.route('/light/off')
+def light_on():
+    if LIGHT.is_active:
+        LIGHT.off()
+        return "turned light off"
+    return "light is already off"
+
+
+@app.route('/light/status')
+def light_status():
+    if LIGHT.is_active:
+        return "light is on"
+    return "light is off"
+
+
+@app.route('/light/blink')
+def light_blink():
+    LIGHT.blink(on_time=1, off_time=1, n=5)
+
 
 if __name__ == '__main__':
    app.run()
