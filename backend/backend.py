2d1c0f4d58374149d662a55a5bf4d2832022af8e
Brent Baude <bbaude@redhat.com>
2017-04-05 18:43:11 +0000

    Additional examples for jack

--- backend/backend.py
+++ backend/backend.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request
 from gpiozero import LED
 
 app = Flask(__name__)
@@ -42,5 +42,30 @@ def light_blink():
     LIGHT.blink(on_time=1, off_time=1, n=5)
 
 
+@app.route('/static/<path>')
+def static_html(path):
+  # send_static_file will guess the correct MIME type
+  return app.send_static_file(path)
+
+
+@app.route('/form/simple', methods=['POST'])
+def simple():
+    return "You submitted simple"
+
+@app.route('/form/form', methods=['POST'])
+def show_values():
+    first = request.form['first']
+    last = request.form['last']
+    gender = request.form['gender']
+    return "{} {} is a {}.".format(first, last, gender)
+
+
+@app.route('/form/form2', methods=['POST'])
+def show_values2():
+    first = request.form['first']
+    last = request.form['last']
+    gender = request.form['gender']
+    return render_template('form_reply.html', first = first, last = last, gender = gender)
+  
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True, host="0.0.0.0")
