e8ec7b2490652f733d0d8f2035c547b74066d5d2
Brent Baude <bbaude@redhat.com>
2017-04-09 20:21:40 +0000

    With Jack's help, added blinking form

--- backend/backend.py
+++ backend/backend.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, session, request
 from gpiozero import LED
+import time
 
 app = Flask(__name__)
 
@@ -7,7 +8,7 @@ LIGHT = LED(17)
 
 def _blink(n):
     for secs in range(n):
-        LIGHT.on() # onn
+        LIGHT.on() # on
         time.sleep(.25)
         LIGHT.off() # off
         time.sleep(.25)
@@ -61,7 +62,7 @@ def simple():
 
 @app.route('/form/form', methods=['POST'])
 def show_values():
-    first = request.form['first']
+    first = request.form['first_name']
     last = request.form['last']
     gender = request.form['gender']
     return "{} {} is a {}.".format(first, last, gender)
@@ -73,6 +74,24 @@ def show_values2():
     last = request.form['last']
     gender = request.form['gender']
     return render_template('form_reply.html', first = first, last = last, gender = gender)
-  
+
+
+@app.route('/form/blink', methods=['POST'])
+def form_blink():
+    bad_input = "We only deal with positive integers"
+    
+    try:
+        num_times_to_blink = int(request.form['n_blink'])
+    except ValueError:
+        return bad_input
+    
+    # Checking for a positive integer greater than 0
+    if num_times_to_blink < 1:
+        return bad_input
+    
+    _blink(num_times_to_blink)
+    return "Done blinking"
+
+ 
 if __name__ == '__main__':
    app.run(debug=True, host="0.0.0.0")
